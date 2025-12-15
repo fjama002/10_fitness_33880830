300e64d4063694eeaeb2d4f27a13c12050ea8bfc
fjama002 <fjama002@gold.ac.uk>
2025-12-15 22:44:05 +0000

    user accounts

--- routes/main.js
+++ routes/main.js
@@ -2,31 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 const shopData = {
-    shopName: "Health & Fitness"
-}
+  shopName: "Health & Fitness",
+};
 
 router.get("/", (req, res) => {
-    res.render("home.ejs", shopData)
-})
+  res.render("home.ejs", shopData);
+});
 
 router.get("/gyms", (req, res) => {
-    res.render("gyms.ejs", shopData)
-})
+  res.render("gyms.ejs", shopData);
+});
 
 router.get("/classes", (req, res) => {
-    res.render("classes.ejs", shopData)
-})
+  res.render("classes.ejs", shopData);
+});
 
 router.get("/aboutus", (req, res) => {
-    res.render("aboutus.ejs", shopData)
-})
+  res.render("aboutus.ejs", shopData);
+});
 
 router.get("/contactus", (req, res) => {
-    res.render("contactus.ejs", shopData)
-})
+  res.render("contactus.ejs", shopData);
+});
 
 router.get("/login", (req, res) => {
-    res.render("login.ejs", shopData)
-})
+  res.render("login.ejs", shopData);
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup.ejs", shopData);
+});
 
 module.exports = router;
